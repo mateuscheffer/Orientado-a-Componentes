ff1c2075b103b5f7a11042697d55fbc47cc64413
Mateus <mateus.scheffer@rede.ulbra.br>
2023-08-23 09:27:01 -0300

    tde

---
+++ projeto/assets/js/script.js
@@ -0,0 +1,7 @@
+function verMaisFotos(){
+    let div = document.querySelector("#moreContent")
+    div.style.display = "block"
+
+    let button = document.querySelector(".button-mais-fotos")
+    button.style.display = "none"
+}
